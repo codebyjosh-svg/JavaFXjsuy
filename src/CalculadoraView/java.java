cf6c549006be9ed5fee05d8ca4d50262b7daba13
Joshua Suy <jsuy-2026083@kinal.edu.gt>
2026-06-12 11:57:44 -0600

    002 Agregue calculadora view

---
+++ src/CalculadoraView/java.java
@@ -0,0 +1,20 @@
+
+package CalculadoraView;
+
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+
+
+public class CalculadoraView {
+    private VBox view;
+    private Label pantalla;
+    private GridPane cuadroBotones;
+    //controller
+    
+    public CalculadoraView(){
+        view = new VBox(15);
+        pantalla = new Label("0");
+        cuadroBotones = new GridPane ();
+    }
+}
